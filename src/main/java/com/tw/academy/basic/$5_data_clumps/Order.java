9170579c6727c093262e701e26bc03424f223229
Eva <eva.dong@cargosmart.com>
2021-06-17 15:32:45 +0800

    move buyerName to user class

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
@@ -4,21 +4,30 @@ public class Order {
 
     private int orderNumber;
 
+    private User user;
     private String buyerName;
     private String buyerPhoneNumber;
     private String buyerAddress;
 
+    public String getBuyerName() {
+        return user.getName();
+    }
+
+    public void setBuyerName(String buyerName) {
+        user = new User(buyerName);
+    }
+
     public Order(int orderNumber, String buyerName, String buyerPhoneNumber, String buyerAddress) {
         this.orderNumber = orderNumber;
 
-        this.buyerName = buyerName;
+        this.setBuyerName(buyerName);
         this.buyerPhoneNumber = buyerPhoneNumber;
         this.buyerAddress = buyerAddress;
     }
 
     public String confirm(){
         return String.format("Please confirm buyer information: buyer name is %s, " +
-                "buyer phone number is %s and buyer address is %s.", buyerName, buyerPhoneNumber, buyerAddress);
+                "buyer phone number is %s and buyer address is %s.", getBuyerName(), buyerPhoneNumber, buyerAddress);
     }
 }
 
---
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/User.java
@@ -0,0 +1,13 @@
+package com.tw.academy.basic.$5_data_clumps;
+
+public class User {
+    private String name;
+
+    public User(String buyerName) {
+        this.name = buyerName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
