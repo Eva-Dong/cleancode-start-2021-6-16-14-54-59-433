8ad068a728b26afcc2876f2b298f76f14ab58984
Eva <eva.dong@cargosmart.com>
2021-06-17 19:27:16 +0800

    refactor MorseCodeDecoder and PigLatin class

--- src/main/java/com/tw/academy/basic/$4_naming/practiceOne/MorseCodeDecoder.java
+++ src/main/java/com/tw/academy/basic/$4_naming/practiceOne/MorseCodeDecoder.java
@@ -1,16 +1,21 @@
 package com.tw.academy.basic.$4_naming.practiceOne;
 
 public class MorseCodeDecoder {
+
+    public static final String REGEX_TWO_AND_MORE_THAN_SPACE = "\\s{2,}";
+    public static final String NULL = "null";
+    public static final String CODE_SEPARATOR = " ";
+
     public static String decode(String input) {
-        String[] tempValue = input.split("\\s{2,}");
-        StringBuilder res = new StringBuilder();
-        for (String s : tempValue) {
-            String [] charArray = s.split(" ");
-            for (String c: charArray) {
-                res.append(MorseCode.get(c));
+        String[] morseCodes = input.split(REGEX_TWO_AND_MORE_THAN_SPACE);
+        StringBuilder decodedMorseCode = new StringBuilder();
+        for (String morseCode : morseCodes) {
+            String [] letters = morseCode.split(CODE_SEPARATOR);
+            for (String letter: letters) {
+                decodedMorseCode.append(MorseCode.get(letter));
             }
-            res.append(" ");
+            decodedMorseCode.append(CODE_SEPARATOR);
         }
-        return res.toString().replaceAll("null", "").trim();
+        return decodedMorseCode.toString().replaceAll(NULL, "").trim();
     }
 }
--- src/main/java/com/tw/academy/basic/$4_naming/practiceTwo/PigLatin.java
+++ src/main/java/com/tw/academy/basic/$4_naming/practiceTwo/PigLatin.java
@@ -1,30 +1,32 @@
 package com.tw.academy.basic.$4_naming.practiceTwo;
 
 public class PigLatin {
-    //Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
     private static final String SUFFIX = "ay";
-    public static String pigIt(String word) {
-        String[] markArrays = {".",",","-",":",";","!","?"};
-        String[] wordArrays = word.split(" ");
-        char firstWord;
+    public static final String SPACE_SEPARATOR = " ";
+
+    public static String pigIt(String sentence) {
+        String[] punctuation = {".",",","-",":",";","!","?"};
+        String[] word = sentence.split(SPACE_SEPARATOR);
+        char firstLetter;
         boolean flag = true;
 
-        for (int i = 0; i < wordArrays.length; i++){
-            for (String markArray : markArrays) {
-                if (wordArrays[i].contains(markArray)) {
+        for (int i = 0; i < word.length; i++){
+            for (String punctuationChar : punctuation) {
+                if (word[i].contains(punctuationChar)) {
                     flag = false;
                     break;
                 }
             }
 
             if (flag){
-                firstWord = wordArrays[i].charAt(0);
-                wordArrays[i] = wordArrays[i].substring(1);
-                wordArrays[i] = wordArrays[i].replace(wordArrays[i], wordArrays[i] + firstWord + SUFFIX);
+                firstLetter = word[i].charAt(0);
+                word[i] = word[i].substring(1);
+                word[i] = word[i].replace(word[i], word[i] + firstLetter + SUFFIX);
             }
             flag = true;
         }
 
-        return String.join(" ", wordArrays);
+        return String.join(SPACE_SEPARATOR, word);
     }
 }
